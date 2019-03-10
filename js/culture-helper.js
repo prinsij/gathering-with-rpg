a278fa5cb00e38acba58c496f41cf4b28c388ac0
ianprins <prinsij@mcmaster.ca>
2019-03-09 21:57:21 -0500

    update version #

--- js/culture-helper.js
+++ js/culture-helper.js
@@ -1,7 +1,7 @@
 $.extend({
 	culture:{
 			loadCulture: function(culture){
-			var version = '1.2015.6.2';
+			var version = '1.2019.3.9';
 			window.localStorage.setItem('culture', culture)
 			switch(culture){
 				case "pt-BR": loadCulture_PT(version); break;
